9e8492a9b4e9e09f0043735e7bcf95fbc6d30bf9
Yoav Dobrin <Yoav.Dobrin@microsoft.com>
2019-12-03 15:30:14 +0000

    revised 1

--- web_main.py
+++ web_main.py
@@ -1,4 +1,4 @@
-from flask import Flask           # import flask
+from flask import Flask           
 app = Flask(__name__)             # create an app instance
 
 @app.route("/")                   # at the end point /
