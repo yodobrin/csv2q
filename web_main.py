97500a49b208fde666a95c65f05472c29a412fcf
Yoav Dobrin <Yoav.Dobrin@microsoft.com>
2019-12-08 06:52:16 +0000

    added an index and a gif

--- web_main.py
+++ web_main.py
@@ -1,8 +1,11 @@
-from flask import Flask           
+from flask import Flask, render_template           
 app = Flask(__name__)             # create an app instance
 
 @app.route("/")                   # at the end point /
-def hello():                      # call method hello
-    return "Hello World!"         # which returns "hello world"
+# def hello():                      # call method hello
+#     return "I like sabich!"         # which returns "hello world"
+def index():
+    return render_template("index.html")
+
 if __name__ == "__main__":        # on running python app.py
     app.run()                     # run the flask app
